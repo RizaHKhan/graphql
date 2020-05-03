290c102003705e38a1275472d3847fc3923d059d
RizaHKhan <khanriza@gmail.com>
2020-05-03 09:37:51 -0400

    Lecture 22: Comment Challenge: Part III

--- basics/index.js
+++ basics/index.js
@@ -37,6 +37,7 @@ const posts = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus cum commodi dignissimos voluptate dolorem porro sed deserunt debitis praesentium!",
     published: true,
     author: "1",
+    comments: '1'
   },
   {
     id: "2",
@@ -45,6 +46,7 @@ const posts = [
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto totam ab similique voluptate hic voluptatibus quia consequuntur sequi delectus pariatur.",
     published: false,
     author: "2",
+    comment: "2",
   },
   {
     id: "3",
@@ -53,6 +55,7 @@ const posts = [
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta labore consequatur voluptates saepe tempore. Perferendis labore dolorem sapiente aperiam ad?",
     published: false,
     author: "3",
+    comment: "3",
   },
 ];
 
@@ -60,22 +63,26 @@ const comments = [
   {
     id: "1",
     text: "This is a commmer about post 1",
-    author: '1'
+    author: "1",
+    post: "1",
   },
   {
     id: "2",
     text: "This is a commmer about post 2",
-    author: '2'
+    author: "2",
+    post: "2",
   },
   {
     id: "3",
     text: "This is a commmer about post 3",
-    author: '3'
+    author: "3",
+    post: "3",
   },
   {
     id: "2",
     text: "This is a commmer about post 2 as well",
-    author: '1'
+    author: "1",
+    post: "2",
   },
 ];
 
@@ -104,12 +111,14 @@ const typeDefs = `
     body: String!
     published: Boolean!
     author: User!
+    comments: [Comment!]!
   }
 
   type Comment {
     id: ID!
     text: String!
     author: User!
+    post: Post!
   }
 `;
 
@@ -166,6 +175,12 @@ const resolvers = {
         return user.id === parent.author;
       });
     },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        console.log(comment.post)
+        return comment.post === parent.id
+      })
+    }
   },
   User: {
     posts(parent, args, ctx, info) {
@@ -175,7 +190,7 @@ const resolvers = {
     },
     comments(parent, args, ctx, info) {
       return comments.filter((comment) => {
-        return comment.author === parent.id
+        return comment.post === parent.id
       })
     }
   },
@@ -184,6 +199,11 @@ const resolvers = {
       return users.find(user => {
         return user.id === parent.author
       });
+    },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => {
+        return post.id === parent.post
+      })
     }
   }
 };
