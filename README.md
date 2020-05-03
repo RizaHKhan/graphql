7fd2283235b8255e5e35b0a86cd61f60225503e4
RizaHKhan <khanriza@gmail.com>
2020-05-03 06:59:14 -0400

    Lecture 14: Creatin Custom Types

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # GRAPHQL
 
-Lecture 10:
+### Lecture 10:
 
 Covered basics of exporting/importing functions between files.
 There can only be one defaul export which can be named anything in the receiving file
 
-Lecture 11:
+### Lecture 11:
 Setup a GraphQL server and query
 
-Lecture 12:
+### Lecture 12:
 Reviewing the five scalar values GraphQL accepts
 
-Lecture 13:
+### Lecture 13:
 Automatic server restarts using nodemon
 Adjusted package.json accordingly
+
+### Lecture 13:
+Query in GraphQL
+```
+query {
+  me {
+    id
+    name
+    email
+    age
+  }
+}
+```
+### Lecture 14:
--- basics/index.js
+++ basics/index.js
@@ -10,32 +10,44 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definitions (schema)
 const typeDefs = `
   type Query {
+    me: User!
+    post: Post!
+  }
+
+  type User {
     id: ID!
     name: String!
-    age: Int!
-    employed: Boolean!
-    gpa: Float!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    id() {
-      return "abc123";
+    me() {
+      return {
+        id: '123456',
+        name: 'khanr',
+        email: 'khanr@gmail.com',
+        age: 34
+      }
     },
-    name() {
-      return "khanr";
-    },
-    age() {
-      return 34;
-    },
-    employed() {
-      return true;
-    },
-    gpa() {
-      return 2.0;
-    },
+    post() {
+      return {
+        id: 'abc',
+        title: 'Post title',
+        body: 'This is post body',
+        published: true
+      }
+    }
   },
 };
 
